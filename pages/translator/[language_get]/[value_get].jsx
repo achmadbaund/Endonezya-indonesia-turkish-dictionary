ef0e818c7e88c6430e9e82ff43b806e93f91a8aa
baund <erdebaund2@gmail.com>
2024-03-20 09:10:53 +0700

    added new theme

--- pages/translator/[language_get]/[value_get].jsx
+++ pages/translator/[language_get]/[value_get].jsx
@@ -84,7 +84,7 @@ export default function TranslatorPage() {
   return (
     <Container>
       <div className="lg:flex w-full bg-gray-100 dark:bg-gray-700-dark text-base md:text-lg py-2 sm:py-0 flex-col sm:flex-row z-[100]">
-        <div className="flex flex-wrap gap-6 pl-[4.5rem] sm:pl-[6rem] pt-2">
+        <div className="flex hide-above-640 flex-wrap gap-6 pl-[4.5rem] sm:pl-[6rem] pt-2">
           <Link 
             href={`/translator/turkish-indonesia/${value ? value : value_get}`}
           >
@@ -116,9 +116,9 @@ export default function TranslatorPage() {
                 <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
                 <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
                 <g id="SVGRepo_iconCarrier">
-                  <path style={{ fill: '#B21727' }} d="M468.324,90.839H43.676c-19.562,0-35.418,15.857-35.418,35.418V256h495.484V126.257C503.742,106.695,487.884,90.839,468.324,90.839z"></path>
-                  <path style={{ fill: '#FFFFFF' }} d="M8.258,385.743c0,19.562,15.857,35.418,35.418,35.418h424.648c19.561,0,35.418-15.857,35.418-35.418V256H8.258V385.743z"></path>
-                  <path style={{ fill: '#121B21' }} d="M468.324,82.581H43.675C19.593,82.581,0,102.174,0,126.257v259.486c0,24.083,19.593,43.676,43.675,43.676h424.648c24.084,0,43.676-19.594,43.676-43.676V126.257C512,102.174,492.407,82.581,468.324,82.581z M495.484,247.742H74.323c-4.561,0-8.258,3.696-8.258,8.258c0,4.562,3.697,8.258,8.258,8.258h421.161v121.485c0,14.977-12.183,27.16-27.16,27.16H43.675c-14.976,0-27.159-12.183-27.159-27.16V264.258H41.29c4.561,0,8.258-3.696,8.258-8.258c0-4.562-3.697-8.258-8.258-8.258H16.516V126.257c0-14.977,12.183-27.16,27.159-27.16h424.648c14.977,0,27.16,12.183,27.16,27.16V247.742z"></path>
+                  <path fill='#B21727' d="M468.324,90.839H43.676c-19.562,0-35.418,15.857-35.418,35.418V256h495.484V126.257C503.742,106.695,487.884,90.839,468.324,90.839z"></path>
+                  <path fill='#FFFFFF' d="M8.258,385.743c0,19.562,15.857,35.418,35.418,35.418h424.648c19.561,0,35.418-15.857,35.418-35.418V256H8.258V385.743z"></path>
+                  <path fill='#121B21' d="M468.324,82.581H43.675C19.593,82.581,0,102.174,0,126.257v259.486c0,24.083,19.593,43.676,43.675,43.676h424.648c24.084,0,43.676-19.594,43.676-43.676V126.257C512,102.174,492.407,82.581,468.324,82.581z M495.484,247.742H74.323c-4.561,0-8.258,3.696-8.258,8.258c0,4.562,3.697,8.258,8.258,8.258h421.161v121.485c0,14.977-12.183,27.16-27.16,27.16H43.675c-14.976,0-27.159-12.183-27.159-27.16V264.258H41.29c4.561,0,8.258-3.696,8.258-8.258c0-4.562-3.697-8.258-8.258-8.258H16.516V126.257c0-14.977,12.183-27.16,27.159-27.16h424.648c14.977,0,27.16,12.183,27.16,27.16V247.742z"></path>
                 </g>
               </svg>
               <span className="ml-2">Indonesia</span>
@@ -141,7 +141,7 @@ export default function TranslatorPage() {
                   >
                     <svg width="100%" height="100%" viewBox="-10.5 -9.45 21 18.9" fill="none" xmlns="http://www.w3.org/2000/svg" className="text-sm me-0 w-10 h-10 text-link dark:text-link-dark flex origin-center transition-all ease-in-out">
                       <circle cx="0" cy="0" r="2" fill="currentColor"></circle>
-                      <g stroke="currentColor" stroke-width="1" fill="none">
+                      <g stroke="currentColor" strokeWidth="1" fill="none">
                         <ellipse rx="10" ry="4.5"></ellipse>
                         <ellipse rx="10" ry="4.5" transform="rotate(60)"></ellipse>
                         <ellipse rx="10" ry="4.5" transform="rotate(120)"></ellipse>
@@ -157,7 +157,7 @@ export default function TranslatorPage() {
               </div>
               
               <div className="flex-col flex mt-0 sm:flex-row w-full md:flex 3xl:w-auto 3xl:shrink-0 3xl:justify-center">
-                <svg className={`h-[1.2rem] absolute top-[1.2rem] left-[1.8rem] sm:left-[5.5rem] lg:left-[6.2rem] sm:top-[1.5rem] text-gray-30 font-bold pointer-events-none`} viewBox="0 0 24 24" fill="#000">
+                <svg className={`h-[1.2rem] hide-below-640 absolute top-[1.2rem] left-[1.8rem] sm:left-[5.5rem] lg:left-[6.2rem] sm:top-[1.5rem] text-gray-30 font-bold pointer-events-none`} viewBox="0 0 24 24" fill="#000">
                   <path d="M16.041 15.856c-0.034 0.026-0.067 0.055-0.099 0.087s-0.060 0.064-0.087 0.099c-1.258 1.213-2.969 1.958-4.855 1.958-1.933 0-3.682-0.782-4.95-2.050s-2.050-3.017-2.050-4.95 0.782-3.682 2.050-4.95 3.017-2.050 4.95-2.050 3.682 0.782 4.95 2.050 2.050 3.017 2.050 4.95c0 1.886-0.745 3.597-1.959 4.856zM21.707 20.293l-3.675-3.675c1.231-1.54 1.968-3.493 1.968-5.618 0-2.485-1.008-4.736-2.636-6.364s-3.879-2.636-6.364-2.636-4.736 1.008-6.364 2.636-2.636 3.879-2.636 6.364 1.008 4.736 2.636 6.364 3.879 2.636 6.364 2.636c2.125 0 4.078-0.737 5.618-1.968l3.675 3.675c0.391 0.391 1.024 0.391 1.414 0s0.391-1.024 0-1.414z"></path>
                 </svg>
                 <input
@@ -171,11 +171,31 @@ export default function TranslatorPage() {
                 />
                 
                 <button
-                  className={`sm:ml-3 mt-2 sm:mt-0 h-[2.5rem] inline-flex justify-center items-center rounded-full text-base py-3 px-5 bg-selected dark:bg-selected-dark text-white dark:text-white`}
+                  className={`sm:ml-3 mt-2 hide-above-640 sm:mt-0 h-[2.5rem] inline-flex justify-center items-center rounded-full text-base py-3 px-5 bg-selected dark:bg-selected-dark text-white dark:text-white`}
                   type="submit"
                 >
                   Translate 
                 </button>
+                <div className="text-base hide-below-640 justify-center items-center gap-1.5 flex 3xl:flex-1 flex-row 3xl:justify-end">
+                  <div className="mx-2.5 gap-1.5 lg:flex">
+                    <div className="flex flex-auto sm:flex-1">
+                      <Link 
+                        className={`active:scale-95 transition-transform w-full text-center outline-link py-1.5 px-1.5 xs:px-3 sm:px-4 capitalize border-b-4 ${language_get === 'turkish-indonesia' ? 'border-selected' : 'border-transparent hover:border-slate-400'}`} 
+                        href={`/translator/turkish-indonesia/${value ? value : value_get}`}
+                      >
+                        Turkish
+                      </Link>
+                    </div>
+                    <div className="flex flex-auto sm:flex-1">
+                      <Link
+                       className={`active:scale-95 transition-transform w-full text-center outline-link py-1.5 px-1.5 xs:px-3 sm:px-4 capitalize border-b-4 ${language_get === 'indonesia-turkish' ? 'border-selected' : 'border-transparent hover:border-slate-400'}`}
+                       href={`/translator/indonesia-turkish/${value ? value : value_get}`}
+                      >
+                        Indonesia
+                      </Link>
+                    </div>
+                  </div>
+                </div>
               </div>
               
             </div>
@@ -192,27 +212,25 @@ export default function TranslatorPage() {
           )}
       </div>
       <div className="mx-auto flex flex-col w-full bg-gradient-right dark:bg-gradient-right-dark" style={{contain: 'content'}}>
-        <div className="flex-col gap-2 flex grow w-full my-10 lg:my-22 mx-auto items-center">
+        <div className="flex-col gap-2 flex grow w-full my-20 lg:my-32 mx-auto items-center">
           <div className="max-w-7xl mx-auto flex flex-col lg:flex-row px-5">
             <div className="max-w-3xl lg:max-w-7xl gap-5 flex flex-col lg:flex-row lg:px-5">
-              <div className="w-full lg:w-9/12 max-w-3xl flex flex-col items-start justify-start lg:ps-5 lg:pe-10">
+              <div className="w-full lg:w-8/12 max-w-3xl flex flex-col items-start justify-start lg:ps-5 lg:pe-10">
                 {translationResults && translationResults.length > 0 && (
                   <div className="flex items-center sm:justify-center sm:ml-0 w-full max-w-3xl">
-                    <table className="table-auto w-full whitespace-nowrap dark:text-slate-900 text-slate-900 text-left border-separate border-spacing-y-2">
-                      <caption className="caption-top mb-3 text-left text-md">
+                    <table className="table-auto w-full whitespace-nowrap font-display text-lg lg:text-lg text-secondary dark:text-secondary-dark text-left border-separate border-spacing-y-2">
+                      <caption className="caption-top mb-3 text-left">
                         Meanings of {`"${value ? value : ''}"`} in : {translationResults.length} result(s)
                       </caption>
                       <thead>
-                        <tr className="bg-slate-900 text-white hide-below-640">
-                          <th className="px-4 py-2 text-md hidden sm:table-cell md:table-cell lg:table-cell xl:table-cell">Category</th>
-                          <th className="px-4 py-2 text-md">Meaning of</th>
-                          <th className="px-4 py-2 text-md">Translation</th>
+                        <tr className="bg-black text-white hide-below-640">
+                          <th className="px-4 py-2">Meaning of</th>
+                          <th className="px-4 py-2">Translation</th>
                         </tr>
                       </thead>
                       <tbody>
                       {translationResults.map((result, index) => (
                         <tr className={`tr-class`} key={index}>
-                          <td className="td-class">Common Usage</td>
                           <td className="td-class">{result.segment}</td>
                           <td className="td-class">{result.translation}</td>
                         </tr>
@@ -223,15 +241,6 @@ export default function TranslatorPage() {
                 )}
               </div>
               <div className="w-full lg:w-3/12">
-                <p className="uppercase tracking-wide font-bold text-sm text-tertiary dark:text-tertiary-dark flex flex-row gap-2 items-center mt-5 lg:-mt-2 w-full">
-                  <svg className="" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20">
-                    <g fill="none" fill-rule="evenodd" transform="translate(-446 -398)">
-                      <path fill="currentColor" fill-rule="nonzero" d="M95.8838835,240.366117 C95.3957281,239.877961 94.6042719,239.877961 94.1161165,240.366117 C93.6279612,240.854272 93.6279612,241.645728 94.1161165,242.133883 L98.6161165,246.633883 C99.1042719,247.122039 99.8957281,247.122039 100.383883,246.633883 L104.883883,242.133883 C105.372039,241.645728 105.372039,240.854272 104.883883,240.366117 C104.395728,239.877961 103.604272,239.877961 103.116117,240.366117 L99.5,243.982233 L95.8838835,240.366117 Z" transform="translate(356.5 164.5)"></path>
-                      <polygon points="446 418 466 418 466 398 446 398"></polygon>
-                    </g>
-                  </svg>
-                  Latest React News
-                </p>
                 <div className="flex-col sm:flex-row flex-wrap flex gap-5 text-start my-5">
                   <div className="flex-1 min-w-[40%] text-start">
                     <p>
@@ -240,15 +249,7 @@ export default function TranslatorPage() {
                       </a>
                     </p>
                   </div>
-                </div>
-                <div className="flex-col sm:flex-row flex-wrap flex gap-5 text-start my-5">
-                  <div className="flex-1 min-w-[40%] text-start">
-                    <p>
-                      <a href="https://auth.upsun.com/register?utm_source=devto&amp;utm_medium=paid_social&amp;utm_campaign=conversion_upsun_debug_faster">
-                        <img src="https://res.cloudinary.com/practicaldev/image/fetch/s--UYzqGtaH--/c_limit%2Cf_auto%2Cfl_progressive%2Cq_100%2Cw_350/https://pro.forem.tools/rails/active_storage/blobs/redirect/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBcEFCIiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--753cca82aef31525b935f311d219aefb5538d5d7/debug-much-faster-1200-1200.png" alt="Billboard image" width="1200" height="1200" loading="lazy" />
-                      </a>
-                    </p>
-                  </div>
+                  
                 </div>
               </div>
             </div>
